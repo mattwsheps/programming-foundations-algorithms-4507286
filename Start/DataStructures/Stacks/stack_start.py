5ca1d03c597375cf63d79701ac5cda6148176389
Matt Gentle-Shepherd <86877770+mattwsheps@users.noreply.github.com>
2024-09-01 13:59:42 +0000

    Implemented a stack in Python

--- Start/DataStructures/Stacks/stack_start.py
+++ Start/DataStructures/Stacks/stack_start.py
@@ -2,12 +2,17 @@
 # try out the Python stack functions
 
 # TODO: create a new empty stack
-
+stack = []
 
 # TODO: push items onto the stack
-
+stack.append(1)
+stack.append(5)
+stack.append(7)
 
 # TODO: print the stack contents
-
+print(stack)
 
 # TODO: pop an item off the stack
+x = stack.pop()
+print(x)
+print(stack)
