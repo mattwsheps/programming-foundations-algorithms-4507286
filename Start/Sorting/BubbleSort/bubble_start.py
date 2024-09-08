4c3ba3b8d54b44ca0c85218be7f8fae76f53d53c
Matt Gentle-Shepherd <86877770+mattwsheps@users.noreply.github.com>
2024-09-08 12:54:05 +0000

    Bubble sort algorithm implemented

--- Start/Sorting/BubbleSort/bubble_start.py
+++ Start/Sorting/BubbleSort/bubble_start.py
@@ -4,8 +4,13 @@
 
 def bubbleSort(dataset):
     # TODO: start with the array length and decrement each time
-
-    print("Current state: ", dataset)
+    for i in range(len(dataset)-1, 0, -1):
+        for j in range(i):
+            if dataset[j] > dataset[j+1]:
+                temp = dataset[j]
+                dataset[j] = dataset[j+1]
+                dataset[j+1] = temp
+        print("Current state: ", dataset)
 
 
 list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
